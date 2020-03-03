22942f78215b7e5ecb12b22c72da41090dcaa0d6
苏杭 <@DESKTOP-1HNUJAF>
2020-03-03 10:51:42 +0800

    提交分支的修改内容 Signed-off-by: 苏杭 <苏杭@DESKTOP-1HNUJAF>

--- src/servlet/StudentInfo.java
+++ src/servlet/StudentInfo.java
@@ -32,7 +32,7 @@ public class StudentInfo extends HttpServlet {
 		List<entity.StudentInfo> lists = sser.isok();
 		//将数据保存到作用域中
 		req.setAttribute("lists",lists);
-		System.out.println(lists);
+		System.out.println(lists+"这是集合");
 //		//页面跳转
 		req.getRequestDispatcher("index.jsp").forward(req, resp);
 	}
